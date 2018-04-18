73bd86911acb5b0e07c77e3bb50e71c1775ca519
Liam Oldfield <liam.oldfield@digital.hmrc.gov.uk>
2018-04-18 12:04:30 +0100

    refactored all tests passing

--- src/main/scala/SumOfSingles.scala
+++ src/main/scala/SumOfSingles.scala
@@ -1,6 +1,6 @@
 object SumOfSingles extends App {
 
-  def apply(input: List[Int]) : Int ={
+  def applya(input: List[Int]) : Int ={
     if (input.length < 2) throw new Exception("invalid input")
     val numberOfRepeats = input.map(x => input.map(y=> x == y).count(_ == true))
     val OnlydistinctNums = input.zip(numberOfRepeats).filter(x => x._2 ==1).map(_._1)
@@ -16,4 +16,8 @@ object SumOfSingles extends App {
 
   }
 
+  def apply(input: List[Int]) :Int ={
+    if (input.length < 2) throw new Exception("invalid input")
+    2*input.distinct.sum-input.sum
+  }
 }
